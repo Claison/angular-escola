a8e4f1b5f55ede780b988e72296a7e19d6dad3a2
claison <claison@hotmail.com.br>
2018-03-05 16:41:07 -0300

    seção1

--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -1,9 +1,14 @@
+import {Professor} from './professor.model';
 export class Disciplina {
+  codigo:number
   nome: string;
   descricao: string;
+  professor:Professor
 
-  constructor(nome: string, descricao?: string) {
+  constructor(codigo:number, nome: string, descricao?: string, professor?:Professor) {
+    this.codigo=codigo
     this.nome = nome;
     this.descricao = descricao;
+    this.professor=professor
   }
 }
